b4415a3823c17fad302cf514b1ce97866921ab32
Santi Ruffino <santiagoruffino@gmail.com>
2023-06-04 00:42:21 -0300

    Fix env and build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   firebase: {
     projectId: 'races-app-534ee',
-    appId: process.env['NG_APP_ID'],
-    databaseURL: process.env['NG_APP_DATABASE_URL'],
+    appId: import.meta.env['NG_APP_ID'],
+    databaseURL: import.meta.env['NG_APP_DATABASE_URL'],
     storageBucket: 'races-app-534ee.appspot.com',
     locationId: 'us-central',
-    apiKey: process.env['NG_APP_API_KEY'],
+    apiKey: import.meta.env['NG_APP_API_KEY'],
     authDomain: 'races-app-534ee.firebaseapp.com',
-    messagingSenderId: process.env['NG_APP_MESSAGING_SENDER_ID'],
-    measurementId: process.env['NG_APP_MEASUREMENT_ID'],
+    messagingSenderId: import.meta.env['NG_APP_MESSAGING_SENDER_ID'],
+    measurementId: import.meta.env['NG_APP_MEASUREMENT_ID'],
   },
   production: true,
 };
